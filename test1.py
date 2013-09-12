4c939ac46304e4e73adc951cbf6a4d124724c354
Tristan Thorman <thormat@ip-10-20-0-135.lan>
2013-09-13 09:40:37 +1000

    Commit for Lulz

--- test1.py
+++ test1.py
@@ -2,3 +2,4 @@ name = raw_input("Please Enter your name: ")
 print("Welcome " + name)
 f = open('~/workfile.txt', 'w'
 f.write(newLine"Hello there, this is a file writing test")
+# just a comment for the sake of commenting okay :) 
